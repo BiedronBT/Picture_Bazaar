764482a519251c0baf81d9afe9cfa33dd0ad4535
Bart <b.jatczak@gmail.com>
2019-07-06 15:03:03 +0200

    Changes to html templates

--- picture_bazaar/picture_bazaar/settings.py
+++ picture_bazaar/picture_bazaar/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_board.apps.MainBoardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
