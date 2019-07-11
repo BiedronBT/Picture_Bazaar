6dc69d8cf9efa44c9cb5060d12823d51bf2f6cfb
Bart <b.jatczak@gmail.com>
2019-07-11 17:59:18 +0200

    Created logic and html for users profile page

--- picture_bazaar/picture_bazaar/urls.py
+++ picture_bazaar/picture_bazaar/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='main-board-home'),
                                                   name='logout'),
+    path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.register, name='register'),
     path('', include('main_board.urls')),
     path('admin/', admin.site.urls),
--- picture_bazaar/users/views.py
+++ picture_bazaar/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, EditUserForm, EditProfileForm
 
 
 # Create your views here.
@@ -10,9 +10,24 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, 'Your account was created! You can now log in.')
+            messages.success(request, 'Your account has been created! You can now log in.')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    if request.method == 'POST':
+        user_form = EditUserForm(request.POST, instance=request.user)
+        profile_form = EditProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your account has been updated.')
+            return redirect('profile')
+
+    user_form = EditUserForm(instance=request.user)
+    profile_form = EditProfileForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
